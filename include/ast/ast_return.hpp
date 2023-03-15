5e19906f06f8cb5073f63a28b9e840e66ffd700b
Bhavya Sharma <107200668+bhavyaEE@users.noreply.github.com>
2023-03-15 11:43:30 +0000

    Update ast_return.hpp

--- include/ast/ast_return.hpp
+++ include/ast/ast_return.hpp
@@ -11,7 +11,7 @@ class Return : public Node{
     ~Return();
 
     void visualiser(std::ostream &os) const override; 
-    void generateRISCV(std::ostream &os, int destReg) const override;
+    void generateRISCV(std::ostream &os, /*Context& context,*/ int destReg) const override;
 
     private: 
     Node* return_expression;
