57a3259bafeceefff3d979e544c5724394e920e8
Bhavya Sharma <107200668+bhavyaEE@users.noreply.github.com>
2023-03-15 11:40:26 +0000

    Update ast_declaration.cpp

--- src/ast/ast_declaration.cpp
+++ src/ast/ast_declaration.cpp
@@ -6,10 +6,10 @@ Declaration::Declaration(std::string _TYPE, Node* _init_declarator)
 void Declaration::visualiser(std::ostream &os) const {
     // implementation here
     os << " " << "Declaration: " <<std::endl;
-    Type->visualiser()<<std::endl;
-    Init_Declarator->visualiser()<<std::endl;
+    Type->visualiser(os)<<std::endl;
+    Init_Declarator->visualiser(os)<<std::endl;
 }
 
-void Declaration::compile_declaration(std::ostream &dst, Context& context, type declarator_type) const {
-    Init_Declarator->generateRISCV(dst, context, destReg);
+void Declaration::compile_declaration(std::ostream &os, /*Context& context,*/ type declarator_type) const {
+    Init_Declarator->generateRISCV(os, /*context,*/ destReg);
 }
