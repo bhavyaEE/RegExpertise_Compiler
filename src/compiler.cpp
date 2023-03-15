9f8ff0048f154edea9457a49ae2368d0f3b93d8f
Bhavya Sharma <107200668+bhavyaEE@users.noreply.github.com>
2023-03-15 11:38:25 +0000

    Update compiler.cpp

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,24 +1,25 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include "ast.hpp"
 
 #include "cli.h"
 
-void compile(std::ostream &w)
-{
-    w << ".text" << std::endl;
-    w << ".globl f" << std::endl;
-    w << std::endl;
+// void compile(std::ostream &w)
+// {
+//     w << ".text" << std::endl;
+//     w << ".globl f" << std::endl;
+//     w << std::endl;
 
-    w << "f:" << std::endl;
-    w << "addi  t0, zero, 0" << std::endl;
-    w << "addi  t0, t0,   5" << std::endl;
-    w << "add   a0, zero, t0" << std::endl;
-    w << "ret" << std::endl;
-}
+//     w << "f:" << std::endl;
+//     w << "addi  t0, zero, 0" << std::endl;
+//     w << "addi  t0, t0,   5" << std::endl;
+//     w << "add   a0, zero, t0" << std::endl;
+//     w << "ret" << std::endl;
+// }
 
 // TODO: uncomment the below if you're using Flex/Bison.
-// extern FILE *yyin;
+extern FILE *yyin;
 
 int main(int argc, char **argv)
 {
@@ -30,15 +31,18 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    
+ 
+
     // TODO: uncomment the below lines if you're using Flex/Bison.
     // This configures Flex to look at sourcePath instead of
     // reading from stdin.
-    // yyin = fopen(sourcePath, "r");
-    // if (yyin == NULL)
-    // {
-    //     perror("Could not open source file");
-    //     return 1;
-    // }
+    yyin = fopen(sourcePath.c_str(), "r");
+    if (yyin == NULL)
+    {
+        perror("Could not open source file");
+        return 1;
+    }
 
     // Open the output file in truncation mode (to overwrite the contents)
     std::ofstream output;
@@ -46,7 +50,10 @@ int main(int argc, char **argv)
 
     // Compile the input
     std::cout << "Compiling: " << sourcePath << std::endl;
-    compile(output);
+    //compile(output);
+    auto root = parseAST();
+    root->visualiser(std::cout);
+    root->generateRISCV(output, 10);
     std::cout << "Compiled to: " << outputPath << std::endl;
 
     output.close();
