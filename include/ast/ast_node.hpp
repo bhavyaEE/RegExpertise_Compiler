f1a4fdaac46076a782b1a1017cdf1348d0bb954e
Bhavya Sharma <107200668+bhavyaEE@users.noreply.github.com>
2023-03-15 11:43:15 +0000

    Update ast_node.hpp

--- include/ast/ast_node.hpp
+++ include/ast/ast_node.hpp
@@ -11,8 +11,7 @@ public:
   virtual ~Node(){}
   //member functions
   virtual void visualiser(std::ostream &os) const = 0; //non-pure virtual as want to be able to print Node value
-  virtual void generateRISCV(std::ostream &os, int destReg) const = 0; //pure virtual as Node does not implement generateRISC
+  virtual void generateRISCV(std::ostream &os, /*Context& context,*/ int destReg) const = 0; //pure virtual as Node does not implement generateRISC
 
 };
-
 
