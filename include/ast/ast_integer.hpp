60be3938ec96b933b8c26ae0638c0f0fe7fb5037
Bhavya Sharma <107200668+bhavyaEE@users.noreply.github.com>
2023-03-15 11:42:57 +0000

    Update ast_integer.hpp

--- include/ast/ast_integer.hpp
+++ include/ast/ast_integer.hpp
@@ -1,22 +1,19 @@
-#ifndef ast_integer.hpp
-#define ast_integer.hpp
+#pragma once
+
 
 #include <string>
 #include <iostream>
+#include "ast_node.hpp"
 
-
-//class Literal : public Expression {};
-class Int
-    : public Expression
+class Int : public Node
 {
-    private:
-        int value;
-    public:
-        Int(int _value){}
-        void visualiser(std::ostream &os) const override;
-        void generateRISCV(std::ostream &os, int destReg) const override;
-	    // type get_data_type(Context& context) const override;
-	    double get_value() const override;
+private:
+    int value;
+public:
+    Int(int _value);
+    ~Int(){}
+    void visualiser(std::ostream &os) const override;
+    void generateRISCV(std::ostream &os, /*Context& context,*/ int destReg) const override;
+	//type get_data_type(Context& context) const override;
+	int get_value() const;
 };
-
-#endif
