7429fd9660e31e83cfeb0a29f8fa22bf41f2e541
Bhavya Sharma <107200668+bhavyaEE@users.noreply.github.com>
2023-03-15 11:39:18 +0000

    Update ast_integer.cpp

--- src/ast/ast_integer.cpp
+++ src/ast/ast_integer.cpp
@@ -3,10 +3,12 @@
 Int::Int(int _value) 
     : value(_value)
 {}
+
 void Int::visualiser(std::ostream &os) const{
     os << " " << "integer = " << value << std::endl;
 }
-void Int::generateRISCV(std::ostream &os, int destReg) const {
+
+void Int::generateRISCV(std::ostream &os, /*Context& context,*/ int destReg) const {
 		// int stack_pointer = context.get_stack_pointer();
 		// std::string destReg = "$2";
 
@@ -15,14 +17,14 @@ void Int::generateRISCV(std::ostream &os, int destReg) const {
 
 		// context.store_register(os, destReg, stack_pointer);
         //needs to figure this out	
-	}
+}
 
 // type INT::get_data_type(Context& context) const { 
 //     return type(INT); //need to have this in context
 // }
+
 int Int::get_value() const { 
     return value; 
 }
-
 
 
